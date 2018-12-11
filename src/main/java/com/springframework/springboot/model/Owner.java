6bfd412f37e606ee1c9abedd639dbc034a8bbb9d
Bengisu Öztürk <bengisu.ozturk95@gmail.com>
2018-12-11 22:51:15 +0300

    Adding POJO classes. Closes #2

---
+++ src/main/java/com/springframework/springboot/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframework.springboot.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springframework/springboot/model/PetType.java
@@ -0,0 +1,14 @@
+package com.springframework.springboot.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
