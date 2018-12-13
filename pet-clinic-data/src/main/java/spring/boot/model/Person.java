0773a18227f93f6950dc8469e85be9b7199b1a2c
Bengisu Öztürk <bengisu.ozturk95@gmail.com>
2018-12-13 22:13:54 +0300

    Arrangement Entities and adding services, closes #5

--- pet-clinic-data/src/main/java/spring/boot/model/Person.java
+++ pet-clinic-data/src/main/java/spring/boot/model/Person.java
@@ -1,6 +1,6 @@
 package spring.boot.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/spring/boot/model/PetType.java
+++ pet-clinic-data/src/main/java/spring/boot/model/PetType.java
@@ -1,6 +1,6 @@
 package spring.boot.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
