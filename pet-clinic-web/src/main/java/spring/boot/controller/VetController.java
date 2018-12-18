0f24e5ce5fcb5e4474306bd872d3ca7af71c1bee
Bengisu Öztürk <bengisu.ozturk95@gmail.com>
2018-12-18 22:50:30 +0300

    For Vets #13

--- pet-clinic-web/src/main/java/spring/boot/controller/VetController.java
+++ pet-clinic-web/src/main/java/spring/boot/controller/VetController.java
@@ -1,13 +1,22 @@
 package spring.boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.boot.service.VetService;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String ListVets(){
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String ListVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
